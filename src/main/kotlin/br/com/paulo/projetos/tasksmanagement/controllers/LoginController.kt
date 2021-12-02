9584c461f3b9fb60a510061c8c701cc8962a0e12
Paulo da Luz <paulo.daluzjr@gmail.com>
2021-12-02 11:35:31 -0300

    feat: creating login route

--- src/main/kotlin/br/com/paulo/projetos/tasksmanagement/controllers/LoginController.kt
+++ src/main/kotlin/br/com/paulo/projetos/tasksmanagement/controllers/LoginController.kt
@@ -1,16 +1,35 @@
 package br.com.paulo.projetos.tasksmanagement.controllers
 
-import org.springframework.web.bind.annotation.GetMapping
+import br.com.paulo.projetos.tasksmanagement.dtos.ErroDTO
+import br.com.paulo.projetos.tasksmanagement.dtos.LoginDTO
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
+import java.lang.RuntimeException
 
 @RestController
 @RequestMapping("api/login")
 class LoginController {
 
-    @GetMapping
-    fun olaMundo() : String {
-        return "Olá mundo"
+    @PostMapping
+    fun efetuarLogin(@RequestBody dto: LoginDTO) : ResponseEntity<Any> {
+        try {
+            if(dto == null || dto.login.isNullOrBlank() || dto.login.isNullOrEmpty()
+                || dto.senha.isNullOrEmpty() || dto.senha.isNullOrBlank()) {
+                return ResponseEntity(ErroDTO(HttpStatus.BAD_REQUEST.value(),
+                    "Parametros inválidos"), HttpStatus.BAD_REQUEST)
+            }
+
+            return ResponseEntity("Login autenticado com sucesso", HttpStatus.OK)
+        } catch(error: Exception) {
+            return ResponseEntity(ErroDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Não foi possivel efetuar o login, tente novamente mais tarde"),
+                HttpStatus.INTERNAL_SERVER_ERROR)
+        }
     }
 
 }
---
+++ src/main/kotlin/br/com/paulo/projetos/tasksmanagement/dtos/LoginDTO.kt
@@ -0,0 +1,3 @@
+package br.com.paulo.projetos.tasksmanagement.dtos
+
+class LoginDTO (val login: String, val senha: String)
