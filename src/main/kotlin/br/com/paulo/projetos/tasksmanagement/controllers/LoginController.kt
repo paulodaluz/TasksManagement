551cd991f24e992d1d95014d0e306911fa392929
Paulo da Luz <paulo.daluzjr@gmail.com>
2021-12-02 14:40:13 -0300

    feat: creating mock to login

--- src/main/kotlin/br/com/paulo/projetos/tasksmanagement/controllers/LoginController.kt
+++ src/main/kotlin/br/com/paulo/projetos/tasksmanagement/controllers/LoginController.kt
@@ -2,6 +2,7 @@ package br.com.paulo.projetos.tasksmanagement.controllers
 
 import br.com.paulo.projetos.tasksmanagement.dtos.ErroDTO
 import br.com.paulo.projetos.tasksmanagement.dtos.LoginDTO
+import br.com.paulo.projetos.tasksmanagement.dtos.LoginRespostaDTO
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -15,16 +16,22 @@ import java.lang.RuntimeException
 @RequestMapping("api/login")
 class LoginController {
 
+    private val LOGIN_TESTE = "admin@admin.com"
+    private val SENHA_TESTE = "Admin1234@"
+
     @PostMapping
     fun efetuarLogin(@RequestBody dto: LoginDTO) : ResponseEntity<Any> {
         try {
             if(dto == null || dto.login.isNullOrBlank() || dto.login.isNullOrEmpty()
-                || dto.senha.isNullOrEmpty() || dto.senha.isNullOrBlank()) {
+                || dto.senha.isNullOrEmpty() || dto.senha.isNullOrBlank()
+                || dto.login != LOGIN_TESTE || dto.senha != SENHA_TESTE
+            ) {
                 return ResponseEntity(ErroDTO(HttpStatus.BAD_REQUEST.value(),
                     "Parametros inválidos"), HttpStatus.BAD_REQUEST)
             }
 
-            return ResponseEntity("Login autenticado com sucesso", HttpStatus.OK)
+            val usuarioTeste = LoginRespostaDTO("Usuário de teste", LOGIN_TESTE);
+            return ResponseEntity(usuarioTeste, HttpStatus.OK)
         } catch(error: Exception) {
             return ResponseEntity(ErroDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "Não foi possivel efetuar o login, tente novamente mais tarde"),
---
+++ src/main/kotlin/br/com/paulo/projetos/tasksmanagement/dtos/LoginRespostaDTO.kt
@@ -0,0 +1,3 @@
+package br.com.paulo.projetos.tasksmanagement.dtos
+
+data class LoginRespostaDTO (val nome: String, val email: String, val token: String = "")
